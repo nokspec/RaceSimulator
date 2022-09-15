b7a168fd87349564bcf40185994b98c4a14e25cd
nokspec <63341284+nokspec@users.noreply.github.com>
2022-09-15 23:19:34 +0200

    15/09/2022

---
+++ Controller/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+	public static class Data
+	{
+        static Competition Competition { get; set; }
+
+        public static void Initialize()
+        {
+            Competition = new Competition();
+        }
+    }
+}
