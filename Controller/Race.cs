64e5252a2d388c5e0555917a3b065e740daa99d6
nokspec <naoki.huisman@gmail.com>
2022-11-27 22:28:50 +0100

    Update Race.cs

--- Controller/Race.cs
+++ Controller/Race.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
